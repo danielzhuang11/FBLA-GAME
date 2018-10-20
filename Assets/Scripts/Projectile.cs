dd5313055d6529ecdcf17e91a76b9144a87a0dc6
Daniel Zhuang <danielzhuang11@hotmail.com>
2018-10-19 19:37:00 -0500

    shooting

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    public float speed;
+    Vector3 mousePosition;
+    Vector3 direction;
+    void Start()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0.0F;
+        direction = (mousePosition - transform.position).normalized;
+        
+    }
+
+    void Update()
+    {
+
+
+        transform.up = direction;
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/SHooting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SHooting : MonoBehaviour {
+
+    public GameObject shot;
+    private Transform playerpos;
+	// Use this for initialization
+	void Start () {
+        playerpos = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(shot, playerpos.position, Quaternion.identity);
+        }
+	}
+}
